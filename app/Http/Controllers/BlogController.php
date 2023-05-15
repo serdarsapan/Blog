6f92c18afea9b71696f410953d45a2df82aad771
serdarsapan <serdarsapan2004@gmail.com>
2023-05-16 02:27:11 +0300

    first commit

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Blog;
+
+class BlogController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $blogs = Blog::latest()->paginate(10);
+        return [
+            "status" => 1,
+            "data" => $blogs
+        ];
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+ 
+        $blog = Blog::create($request->all());
+        return [
+            "status" => 1,
+            "data" => $blog
+        ];
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Blog $blog)
+    {
+        return [
+            "status" => 1,
+            "data" =>$blog
+        ];
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Blog $blog)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Blog $blog)
+    {
+        $request->validate([
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+ 
+        $blog->update($request->all());
+ 
+        return [
+            "status" => 1,
+            "data" => $blog,
+            "msg" => "Blog updated successfully"
+        ];
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Blog $blog)
+    {
+        $blog->delete();
+        return [
+            "status" => 1,
+            "data" => $blog,
+            "msg" => "Blog deleted successfully"
+        ];
+    }
+}
